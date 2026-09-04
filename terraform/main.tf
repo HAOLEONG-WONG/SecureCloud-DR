3d7753a67eddc58ee96e96e45bbd4385a2ab79f6
HAO LEONG WONG <hlwong2005@gmail.com>
2026-09-04 15:34:57 +0800

    feat: import Resource Group, VNet, Subnet into Terraform management

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "rg-securecloud-dr-lab"
+  location = "Malaysia West"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "vnet-securecloud-dr"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "web" {
+  name                 = "snet-web"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.0.0/24"]
+  default_outbound_access_enabled = false
+}
