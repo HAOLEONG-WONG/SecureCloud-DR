66b68c3450ff20b0af001109018643887905bef7
HAO LEONG WONG <hlwong2005@gmail.com>
2026-09-08 17:32:29 +0800

    feat: import Storage Account into Terraform management

--- terraform/main.tf
+++ terraform/main.tf
@@ -132,3 +132,16 @@ resource "azurerm_linux_virtual_machine" "web" {
     ignore_changes = [admin_ssh_key]
   }
 }
+
+resource "azurerm_storage_account" "main" {
+  name                     = "stsecuredr01"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  allow_nested_items_to_be_public  = false
+  min_tls_version                  = "TLS1_2"
+  cross_tenant_replication_enabled = false
+
+}
